4fd44e5a21e3048f8419fbcba01999e86c7a233d
MouricManon <manon.mouric@etud.univ-jfc.fr>
2022-06-08 12:13:52 +0200

    last commit ;(
    ;-( ;-(

--- frontend/src/components/TableauDemande.vue
+++ frontend/src/components/TableauDemande.vue
@@ -11,7 +11,7 @@ const data = reactive({
     id: "",
     demandes: [],
 demandesSuppr:[],
-Demande d,
+
 });
 
 function getDemandes() {
@@ -36,7 +36,7 @@ function getDemandes() {
 d.setDate(datemodif),
 console.log(datemodif), 
  )
-}*/
+}
 function deleteFetch(id) {
     let myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
@@ -47,7 +47,7 @@ function deleteFetch(id) {
     fetch("/api/demandes/" + id, fetchOptions)
         .then(() => {
             fetchDemandes();
-           affichageTraitement();
+           //setTraite(true);
         })
         .catch((error) => alert(error));
 }
@@ -59,13 +59,8 @@ function fetchDemandes() {
         })
         .catch((error) => alert(error));
 }
+*/
 
-function affichageTraitement(){
-  for(d in data.demandes){
-d.setTraite(true);
-console.log(d.getTraite());
-  }
-}
 </script>
 
 
@@ -94,7 +89,7 @@ console.log(d.getTraite());
                                 <td>{{ demande.objet }}</td>
                                 <td class="info">{{ demande.profession }}</td> 
                                 <td class="voir">
-                                    <a href="/Voir" class="text-decoration-none txt-bleufonce" id="voir" @click="$emit('laDemandeid', id)"> Voir</a>
+                                    <button value=demande.id class="btn-btn-primary" id="voir" @click="$emit('laDemandeid', $event.target.value)"> Voir</button>
                                 </td>       
                                 <td>
                                  <ConfirmationTraitement @TraitementConfirmed="deleteFetch" :id="demande.id" /> </td>   </tr>
@@ -122,74 +117,3 @@ console.log(d.getTraite());
   color :rgb(46, 129, 197);
 }
 </style>
-<!--Case : Tableau <template>
-    <div class="tableau">
-      <h2>Liste des médicaments en cours d'ajout</h2>
-
-      <table border="1" id="leTableau" class="table table-bordered table-sm table-hover">
-        <thead>
-          <tr>
-            <th colspan="4" id="liste">
-              Liste de vos médicaments en cours de saisie
-            </th>
-            <th id="action">Réécrire une saisie</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(ceTraitement, index) in listTraitement" :key="index">
-            <td id="1">
-              nomdemandeur du médicament : <br />
-              Maladie traité :
-            </td>
-            <td id="2">
-              {{ ceTraitement._medic }} <br />
-              {{ ceTraitement._maladie }}
-            </td>
-            <td id="3">
-              Durée de prise : <br />
-
-              Fréquence de prise : <br />
-
-              Quantité à prendre :
-            </td>
-            <td id="4">
-              {{ ceTraitement._duree }} {{ ceTraitement._unitduree }} <br />
-
-              {{ ceTraitement._freq }} / {{ ceTraitement._unitfreq }} <br />
-
-              {{ ceTraitement._qte }} / Prise<br />
-            </td>
-          </tr>
-        </tbody>
-      </table>
-
-    
-      <input type="button" id="enregistrer" value="Valider la liste et l'enregistrer" @click="putDemande()" />
-    </div>
-  </div></template>
-  function getFrequence(event) {
-  let url = "/api/allUniteFreq";
-  let fetchOptions = { method: "Get" };
-  fetch(url, fetchOptions)
-    .then((response) => {
-      return response.json();
-    })
-    .then((dataJSON) => {
-      dataJSON.forEach((v) => listeunitFreq.push(v));
-    })
-    .catch((error) => { });
-}
-
-function getDuree(event) {
-  let url = "/api/allUniteDuree";
-  let fetchOptions = { method: "Get" };
-  fetch(url, fetchOptions)
-    .then((response) => {
-      return response.json();
-    })
-    .then((dataJSON) => {
-      dataJSON.forEach((v) => listeunitDuree.push(v));
-    })
-    .catch((error) => { });
-}*/-->  
-   <!-- Bouton -->
--- frontend/src/views/Resoudre.vue
+++ frontend/src/views/Resoudre.vue
@@ -1,12 +1,27 @@
 <script setup>
 import TableauDemande from "@/components/TableauDemande.vue";
+import { json } from "body-parser";
+import VoirDemande from "@/components/VoirDemande.vue";
+/*
+function handler(id){
+  let fetchOptions={method:"GET"};
+  fetch("api/api/demandes/"+id)
+  .then((response)=> response.json())
+  .then((json)=>{
+    let results=json._embedded.demandes,
+    results.forEach(v)=>data.demandes.push(v),
 
+    console.log(json),
+  })
+}*/
 </script>
 <template>
   
   <div class="espace">
-<TableauDemande>
+<TableauDemande @laDemandeid="handler">
 </TableauDemande>
+<VoirDemande>
+</VoirDemande>
 </div>
 
 </template>
